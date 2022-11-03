a4ead58897ddbbf545e97a9210c6e4ed95866d88
browynlouis <browynlouis2@gmail.com>
2022-11-03 11:14:46 -0700

    styles: added styles for inputfield and button components

---
+++ src/components/InputField.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Input = ({ id, type, label, placeholder, helperText }) => {
+  return (
+    <>
+      <div className="link-tree-inputContainer">
+        {label ? <label htmlFor={id}>{label}</label> : null}
+        <input
+          type={type}
+          id={id}
+          placeholder={placeholder}
+          className={`link-tree-input`}
+        />
+        {helperText ? (
+          <span className="link-tree-input-helperText">{helperText}</span>
+        ) : null}
+      </div>
+    </>
+  );
+};
+
+export default Input;

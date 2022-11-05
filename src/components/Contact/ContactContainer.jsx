388203f2402bd45b4973b2dd3ca2088b3d5a9579
browynlouis <browynlouis2@gmail.com>
2022-11-05 09:16:18 -0700

    chore: maintenance on contact container component

--- src/components/Contact/ContactContainer.jsx
+++ src/components/Contact/ContactContainer.jsx
@@ -4,9 +4,16 @@ import ContactHeader from "./ContactHeader";
 import ContactForm from "./ContactForm";
 
 const ContactContainer = () => {
+  const scrollTopRef = React.useRef();
+
+  React.useEffect(() => {
+    if (scrollTopRef.current)
+      scrollTopRef.current.scrollIntoView({ behavior: "smooth" });
+  }, []);
+
   return (
     <>
-      <div className="link-tree-contactContainer">
+      <div className="link-tree-contactContainer" ref={scrollTopRef}>
         <div className="link-tree-contactContainer-content">
           <ContactHeader />
           <ContactForm />

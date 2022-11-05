04bca783f88e79409e8991b9b3603a47b2f85134
browynlouis <browynlouis2@gmail.com>
2022-11-05 10:29:21 -0700

    refactor: little changes

--- src/components/Home/Card/CardContainer.jsx
+++ src/components/Home/Card/CardContainer.jsx
@@ -4,9 +4,15 @@ import CardHeader from "./CardHeader";
 import CardBody from "./CardBody";
 
 const CardContainer = () => {
+  const scrollTopRef = React.useRef();
+
+  React.useEffect(() => {
+    if (scrollTopRef.current)
+      scrollTopRef.current.scrollIntoView({ behavior: "smooth" });
+  }, []);
   return (
     <>
-      <div className="link-tree-cardContainer">
+      <div className="link-tree-cardContainer" ref={scrollTopRef}>
         <div className="link-tree-cardContainer-content">
           <CardHeader />
           <CardBody />

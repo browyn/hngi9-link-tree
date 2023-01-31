d3cc50f8b0279e7214ecc154ce747886b80d3b09
Browyn Louis <browynlouis2@gmail.com>
2023-01-31 09:17:08 +0100

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
+import Header from "./Header";
 import Footer from "./Footer";
 
 const Layout = ({ children }) => {
+  const location = useLocation();
+  
   return (
     <>
+      {location.pathname === "/account" ? null : <Header />}
       <main>
         <div className="link-tree-pageContent" id="">
           {children}
